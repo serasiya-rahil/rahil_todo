6dad18ad3157ccda2bd537b940880d9619934ac5
serasiya-rahil <138242322+serasiya-rahil@users.noreply.github.com>
2023-07-05 15:00:14 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,19 +1,53 @@
-from app.forms import TODOForm
-from app.models import TODO
+from app.forms import TODOForm #imported
+from app.models import TODO # imported
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login as loginUser, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm #imported
+from django.contrib.auth import authenticate, login as loginUser, logout #imported
+from django.contrib.auth.decorators import login_required #imported
+from app.log import setup_logger as log, log_function_name
+
+
+
+#setup log function to keep application tracking
+logger = log("./logs/app.log")
 
 @login_required(login_url='login')
 def home(request):
     
     if(request.user.is_authenticated): 
+       
+     
+
         user = request.user
-        todos = TODO.objects.filter(user = user).order_by('priority')
+        todos = TODO.objects.filter(user = user).order_by('-status')
+
+        pending_count = TODO.objects.filter(user=user, status__in=['Pending', 'In Progress']).count()
+
+        Complete_count = TODO.objects.filter(user = user, status='Completed').count()
+        Total_count = TODO.objects.filter(user = user).count()
+        if(Total_count or Complete_count):
+
+            percentage = (Complete_count / Total_count) * 100
+            percentage = round(percentage,2)
+        else:
+            percentage=0
+
         form = TODOForm()
-        return render(request, 'index.html', context = {'form': form, 'todos': todos })
+
+        context ={ 'form': form,
+                 'todos': todos ,
+                 'pending_count': pending_count,
+                 'Complete_count': Complete_count,
+                 'percentage':percentage,
+                 'user': user
+            }
+        return render(request, 'index.html', context = context)
+    else:
+        form = AuthenticationForm()
+        context ={
+            "form":form}
+        return render(request, 'login.html', context=context)
 
 def login(request):
     if(request.method == 'GET'):
@@ -33,6 +67,7 @@ def login(request):
                 loginUser(request, user)
                 return redirect('home')
         else:
+            logger.error(f"{log_function_name(logger)} - {request.user} username or password is wrong | Redirecting to Login Page")
             context ={
                 "form":form
             }
@@ -47,7 +82,7 @@ def signup(request):
             }
         return render(request, 'singup.html', context = context)
     else:
-        print(request.POST)
+        #print(request.POST)
         form = UserCreationForm(request.POST)
         context = {
             "form": form
@@ -58,6 +93,9 @@ def signup(request):
                 return redirect('login')
 
         else:
+            #Log The error
+            logger.error(f"{log_function_name(logger)} - {request.user} User Creation Form is Invalid | Try signing in again")
+            #log ends
             return render(request, 'singup.html', context = context)
 
 #add the todo list
@@ -67,16 +105,16 @@ def add_todo(request):
     #check if user is logged in or not
     if(request.user.is_authenticated): 
         user = request.user
-        print("User: ",user)
+        #print("User: ",user)
         form = TODOForm(request.POST)
         
         todo = form.save(commit=False)
         todo.user = user
         todo.save()
-        print(todo)
-        print(f"Todo :{todo}")
+        #print(todo)
+        #print(f"Todo :{todo}")
         if(form.is_valid()):
-            print(form.cleaned_data) 
+            #print(form.cleaned_data) 
             return redirect('home')
         else:
             return render(request, 'index.html', context = {'form': form})
