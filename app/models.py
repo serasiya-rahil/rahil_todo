3268714781490ffaaaacd681e87dad9a67ea4c1f
serasiya-rahil <138242322+serasiya-rahil@users.noreply.github.com>
2023-07-05 15:01:21 -0400

    Update models.py
    Added In Progress Task

--- app/models.py
+++ app/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 
 
 class TODO(models.Model):
-	
+
 	status_choices = [
     ("Completed", "Completed"),
     ("Pending", "Pending"),
+    ("In Progress","In Progress")
 	]
 
 	priority_choices = [
@@ -22,11 +23,11 @@ class TODO(models.Model):
     ("10", "10 (Lowest)")
 
 	]
-	
+
 
 	title = models.CharField(max_length=30)
 	description = models.CharField(max_length=200, blank=True, null=True)
-	status = models.CharField(max_length = 10, choices=status_choices)
+	status = models.CharField(max_length = 20, choices=status_choices)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	date = models.DateTimeField(auto_now_add=True)
 	priority = models.CharField(max_length=2, choices=priority_choices)
